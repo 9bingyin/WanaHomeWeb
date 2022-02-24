c61728de204e778264d93691c09dba752bd44c03
⑨bingyin <37502927+9bingyin@users.noreply.github.com>
2022-02-25 03:05:17 +0800

    Update ServerList.vue

--- wana_home_front/src/views/ServerList.vue
+++ wana_home_front/src/views/ServerList.vue
@@ -2,23 +2,30 @@
     <div style="max-width: 90%;width: fit-content;">
         <b-row align-h="between" cols="2">
             <b-col md="auto">
-                <p>此处应有标题</p>
+                <p>冰音のWanaHomeWeb</p>
             </b-col>
             <b-col class="text-right" md="auto">
-                <b-btn size="sm" variant="outline-info" @click="update()">
+                <b-btn size="sm" variant="outline-info umami--click--refresh-button" @click="update()">
                     <b-icon-arrow-clockwise/>
                 </b-btn>
             </b-col>
         </b-row>
         <b-row cols="1" cols-md="2">
             <b-col class="text-center">
-                此处应有文字
+                <p><b>该网站所有数据均为玩家自发更新</b></p>
+                <p>原项目：<a href="https://github.com/nyaoouo/WanaHomeWeb" target="_blank">https://github.com/nyaoouo/WanaHomeWeb</a></p>
+                <p><a href="https://wanahomeweb.statuspage.io" target="_blank">状态页</a>（服务状态监控及维护通知）</p>
+                <p>当前版本：国服5.58</p>
+                <p>使用方式可查看NGA<a href="https://ngabbs.com/read.php?tid=28626126" target="_blank">原帖</a></p>
+                <p>令牌下载链接：<a href="https://bingyintech.coding.net/s/dba35ecc-92d0-47fa-8049-6120465b5081" target="_blank">点击下载</a></p>
+                <p>数据库公开下载链接（每天凌晨3点更新数据）：<a href="https://1drv.ms/u/s!AlHesNgJkUQuswYWUhoJ2saux4qx?e=eZsIEn" target="_blank">点击下载</a></p>
+                <p>冰音の博客：<a href="https://9bingyin.com" target="_blank">https://9bingyin.com</a></p>
             </b-col>
             <b-col>
                 <b-col v-for="dc in dc_server" :key="dc.dc_name" class="shadow-sm rounded m-2 p-2">
                     {{ dc.dc_name }}
                     <div>
-                        <b-btn class="m-1" v-for="(w_name,w_id) in dc.servers"
+                        <b-btn class="m-1 umami--click--dc-button" v-for="(w_name,w_id) in dc.servers"
                                :key="w_id" size="sm" variant="success" :disabled="!(w_id in servers)"
                                v-b-tooltip.hover.top :title="`最近更新：${time_diff(servers[w_id],false)}前`"
                                @click="$router.push({name:'State',params:{server:w_id}})">
